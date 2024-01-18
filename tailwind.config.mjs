291e86ceecfffd52307a574e239df1d0176b6a96
Uriel Jolodovsky <46960341@est.ort.edu.ar>
2024-01-18 00:13:02 -0300

    About Me section started

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,20 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+			radiusMorph: {
+				"0%": { borderRadius: "56% 44% 42% 58% / 51% 55% 45% 49%" },
+				"25%": { borderRadius: "41% 59% 36% 64% / 61% 55% 45% 39%" },
+				"50%": { borderRadius: "59% 41% 53% 47% / 46% 48% 52% 54%" },
+				"75%": { borderRadius: "49% 51% 38% 62% / 58% 51% 49% 42%" },
+				"100%": { borderRadius: "56% 44% 42% 58% / 51% 55% 45% 49%" },
+			  },
+			},
+		},
+		animation: {
+			radiusMorph: "radiusMorph 10s infinite",
+		},
 	},
 	plugins: [],
 }
