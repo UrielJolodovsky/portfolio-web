83628123aae1d0c1803405ecbee332401c0f26c9
Uriel Jolodovsky <46960341@est.ort.edu.ar>
2024-01-31 12:50:35 -0300

    fix astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://urijolo.me/'
+  site: 'https://urijolo.me/',
   output: "static",
   integrations: [tailwind()]
 });
